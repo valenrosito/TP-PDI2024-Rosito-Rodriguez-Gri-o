4b4a7ad41abcdc439eda90e1104be87d95f370ec
valenrosito <valenrosito9@gmail.com>
2024-11-22 14:57:29 -0300

    Resolución de los puntos A) y B) del problema 1

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -1,32 +1,56 @@
-import cv2
-import numpy as np
-from tkinter import Tk, Scale, HORIZONTAL
-from tkinter import Label
-from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
 
-imagen = cv2.imread('TP2/monedas.jpg', cv2.IMREAD_GRAYSCALE)
+# Cargamos la imagen y le aplicamos un filtro de desenfoque para eliminar el ruido
+image = cv2.imread('TP2/monedas.jpg')
+image = cv2.blur(image, (7, 7))
 
-# Aplicamos desenfoque para reducir el ruido y facilitar la deteccion de bordes con Canny
-imagen_suave = cv2.GaussianBlur(imagen, (3, 3), 0)
-bordes = cv2.Canny(imagen_suave, 30, 190)
+# Convertimos la imagen a hls y aplicamos sobre el canal de saturacion un filtro de desenfoque 
+hls = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+h, l, s = cv2.split(hls)
+image_blureada = cv2.blur(s, (3, 3))
 
-# Aplicamos dilatación para conectar bordes
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-bordes_dilatados = cv2.morphologyEx(bordes, cv2.MORPH_DILATE, kernel, iterations=3)
-bordes_dilatados = cv2.morphologyEx(bordes_dilatados, cv2.MORPH_CLOSE, kernel, iterations=2)
+# Aplicamos un umbral para obtener una imagen binaria y poder realizar operaciones morfologicas
+_, imagen_binaria = cv2.threshold(image_blureada, 12, 255, cv2.THRESH_BINARY)
 
-# Creamos una máscara usando los bordes detectados y luego la aplicamos en la imagen original
-mascara_objetos = cv2.bitwise_not(bordes_dilatados)
-resultado = cv2.bitwise_and(imagen, imagen, mask=mascara_objetos)
 
-f = mascara_objetos
-fd = cv2.morphologyEx(f, cv2.MORPH_DILATE, kernel)
-fo = cv2.morphologyEx(f, cv2.MORPH_OPEN, kernel)
-fc = cv2.morphologyEx(f, cv2.MORPH_CLOSE, kernel)
-plt.figure()
-ax1 = plt.subplot(221); plt.imshow(f, cmap='gray'), plt.title('Imagen Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(222,sharex=ax1,sharey=ax1), plt.imshow(fd, cmap='gray'), plt.title('Dilatacion'), plt.xticks([]), plt.yticks([])
-plt.subplot(223,sharex=ax1,sharey=ax1), plt.imshow(fo, cmap='gray'), plt.title('Apertura'), plt.xticks([]), plt.yticks([])
-plt.subplot(224,sharex=ax1,sharey=ax1), plt.imshow(fc, cmap='gray'), plt.title('Clausura'), plt.xticks([]), plt.yticks([])
-plt.show(block=False)
+matriz = cv2.getStructuringElement(cv2.MORPH_RECT, (40, 40))
+clausura = cv2.morphologyEx(imagen_binaria, cv2.MORPH_CLOSE, matriz)
+apertura = cv2.morphologyEx(clausura, cv2.MORPH_OPEN, matriz)
+
+matriz = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+erode = cv2.erode(apertura, matriz)
+
+
+contornos, _ = cv2.findContours(erode, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+mascara = np.zeros_like(erode, dtype=np.uint8)
+
+monedas = []
+dados = []
+for contorno in contornos:
+    area = cv2.contourArea(contorno)
+    perimetro = cv2.arcLength(contorno, True)
+    fp = area / (perimetro**2)
+    if fp > 0.06:
+        monedas.append((contorno, area))
+    else:
+        dados.append((contorno, area))
+
+# Ordenamos la lista de monedas en base a su area para facilitar la visualziacion
+sorted_monedas = sorted(monedas , key=lambda x: x[1])
+ten_cents = 0
+fifty_cents = 0
+one_peso = 0
+
+for moneda in sorted_monedas:
+    if moneda[1] > 95000:
+        fifty_cents += 1
+    elif moneda[1] < 65000:
+        ten_cents += 1
+    elif moneda[1] < 85000 and moneda[1] > 65000:
+        one_peso += 1
+
+print(f"Se encontraron las siguientes cantidades de monedas:\nMonedas 10 centavos: {ten_cents}\nMonedas 50 centavos: {fifty_cents}\nMonedas 1 peso: {one_peso}")
+
+
