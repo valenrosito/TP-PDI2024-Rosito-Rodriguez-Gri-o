b0cb335dc32004887fd32feeabf539d48387b675
valenrosito <valenrosito9@gmail.com>
2024-12-04 20:04:42 -0300

    Dios, bendecinos

--- TP3/dados.py
+++ TP3/dados.py
@@ -69,10 +69,14 @@ def detectar_dados(frame):
     closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
 
     contours, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mascara = np.zeros_like(gray)
+    cv2.drawContours(mascara, contours, -1, 255, -1)
+
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mascara, connectivity=8)
     bounding_boxes = []
 
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
+    for i in range(1, num_labels):  # Empezar desde 1 para ignorar el fondo
+        x, y, w, h, area = stats[i]
         if 70 <= w <= 100 and 70 <= h <= 100:
             bounding_boxes.append((x, y, w, h))
 
@@ -87,11 +91,14 @@ def dibujar_bounding_boxes(frame, bounding_boxes):
 def mostrar_video_con_bounding_boxes(video_path):
     """Procesa el video y muestra los bounding boxes en cada fotograma."""
     cap = cv2.VideoCapture(video_path)
-    global width, height
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS)) 
 
     frame_count = 0
+
+    out = cv2.VideoWriter('Output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -101,15 +108,18 @@ def mostrar_video_con_bounding_boxes(video_path):
         if frame_count >= 30:
             frame_with_boxes = dibujar_bounding_boxes(frame, bounding_boxes)
             frame_with_boxes = cv2.resize(frame_with_boxes, (int(width / 3), int(height / 3)))
+            out.write(frame_with_boxes)
             cv2.imshow('Dados', frame_with_boxes)
+            
         else: 
             cv2.imshow('Dados', frame)
-
+            
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
         frame_count += 1  # Incrementar el contador de fotogramas
 
 # os.makedirs("TP3/frames", exist_ok=True)
+os.makedirs("TP3/videos_outpu", exist_ok=True)
 # procesar_video('TP3/videos/tirada_1.mp4')  
 
-mostrar_video_con_bounding_boxes('TP3/videos/tirada_4.mp4')  
+mostrar_video_con_bounding_boxes('TP3/videos/tirada_2.mp4')  
